0d80614d0a511cf1a3a2a928900e942451e32ed1
jkljabo <jason.k.little@comcast.net>
2026-07-01 14:52:14 -0400

    Initial Setup and Release V1.0 Theme changes.

---
+++ Components/Services/ThemeService.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorCodeChallenge.Components.Services
+{
+    public class ThemeService
+    {
+        public string CurrentTheme { get; private set; } = "gray";
+
+        public event Action? ThemeChanged;
+
+        public void SetTheme(string theme)
+        {
+            CurrentTheme = theme;
+            ThemeChanged?.Invoke();
+        }
+    }
+}
